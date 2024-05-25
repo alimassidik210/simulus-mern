dfc2ac9f4175aca8ed2da664f60a6057e1bc101f
alimassidik210 <alim.assidik210@gmail.com>
2024-05-25 09:47:30 +0700

    fix complete app

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
 import listingRoute from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 const app = express();
 
@@ -17,12 +18,18 @@ mongoose
   })
   .catch((error) => console.log(error));
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/listing", listingRoute);
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
